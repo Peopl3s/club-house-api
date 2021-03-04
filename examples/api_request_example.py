3484822085876586bd629bb96756483aa581a44d
Peopl3s <nikposd@mail.ru>
2021-03-04 10:46:28 +0300

    Update api_request_example.py

--- examples/api_request_example.py
+++ examples/api_request_example.py
@@ -1,1 +1,19 @@
-pass
+from club_house_api import ClubHouse
+import asyncio
+
+TOKEN = os.getenv('API_TOKEN')
+
+club_house_session = ClubHouse(TOKEN, 'v3')
+club_house = club_house_session.get_api()
+
+async def main():
+
+    all_projects = await club_house.projects()
+    first_project_id = all_projects[0]['id']
+
+    new_story = {'name': 'My new story', 'project_id': first_project_id}
+    story = await club_house.stories.create(**new_story)
+    print(story)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
